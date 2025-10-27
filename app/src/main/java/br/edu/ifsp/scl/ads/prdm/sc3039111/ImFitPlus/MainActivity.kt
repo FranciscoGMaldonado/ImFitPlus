8be0b622bacf305d6c2bb44443365682db8ec816
Francisco <francisco.guaturamaldonado83@gmail.com>
2025-10-27 20:29:25 -0300

    feat: intent da tela inicial para a tela de dados pessoais

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039111/ImFitPlus/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039111/ImFitPlus/MainActivity.kt
@@ -1,20 +1,22 @@
 package br.edu.ifsp.scl.ads.prdm.sc3039111.ImFitPlus
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.prdm.sc3039111.ImFitPlus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnComece.setOnClickListener {
+            val intent = Intent(this, DadosContaActivity::class.java)
+            startActivity(intent)
         }
     }
 }
