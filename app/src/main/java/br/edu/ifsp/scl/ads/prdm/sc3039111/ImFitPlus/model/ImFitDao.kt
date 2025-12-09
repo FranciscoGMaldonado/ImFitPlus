30c652c21807dc11f1842a0963b0401992e35c64
Francisco Guatura Maldonado <francisco.guaturamaldonado83@gmail.com>
2025-12-08 22:31:27 -0300

    feat(model): funções na interface para histórico de cálculos

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039111/ImFitPlus/model/ImFitDao.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039111/ImFitPlus/model/ImFitDao.kt
@@ -8,6 +8,10 @@ import androidx.room.Query
 interface ImFitDao {
     @Insert
     fun insertUser(user: User): Long
+    @Insert
+    fun insertHistory(history: History): Long
     @Query("SELECT * FROM User WHERE id = :id")
     fun getUserById(id: Int): User?
+    @Query("SELECT * FROM History WHERE id = :id")
+    fun getHistoryById(id: Int): History?
 }
