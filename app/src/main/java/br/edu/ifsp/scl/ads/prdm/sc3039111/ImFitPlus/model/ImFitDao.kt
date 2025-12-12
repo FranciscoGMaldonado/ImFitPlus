24449dce46b46a58a93aac454d4e79719834d70f
Francisco Guatura Maldonado <francisco.guaturamaldonado83@gmail.com>
2025-12-12 01:56:43 -0300

    refactor(model): refatoração para o parão e SQLite

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039111/ImFitPlus/model/ImFitDao.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039111/ImFitPlus/model/ImFitDao.kt
@@ -1,17 +1,9 @@
 package br.edu.ifsp.scl.ads.prdm.sc3039111.ImFitPlus.model
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-
-@Dao
 interface ImFitDao {
-    @Insert
     fun insertUser(user: User): Long
-    @Insert
     fun insertHistory(history: History): Long
-    @Query("SELECT * FROM User WHERE id = :id")
     fun getUserById(id: Int): User?
-    @Query("SELECT * FROM History WHERE id = :id")
     fun getHistoryById(id: Int): History?
+    fun getAllHistory(): List<History>
 }
--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039111/ImFitPlus/model/User.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039111/ImFitPlus/model/User.kt
@@ -1,14 +1,11 @@
 package br.edu.ifsp.scl.ads.prdm.sc3039111.ImFitPlus.model
 
 import android.os.Parcelable
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-@Entity
 data class User(
-    @PrimaryKey(autoGenerate = true) var id: Int? = -1,
+    var id: Int? = -1,
     var nome: String = "",
     var idade: Int = -1,
     var sexo: String = "",
