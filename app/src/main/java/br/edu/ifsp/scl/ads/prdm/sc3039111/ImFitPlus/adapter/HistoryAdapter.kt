be735bcf8734ccdeaaea65a1f710a7bf74c40b11
Francisco Guatura Maldonado <francisco.guaturamaldonado83@gmail.com>
2025-12-12 02:02:58 -0300

    feat(adapter): criação e adapter para activity de Histórico

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039111/ImFitPlus/adapter/HistoryAdapter.kt
@@ -0,0 +1,43 @@
+package br.edu.ifsp.scl.ads.prdm.sc3039111.ImFitPlus.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.edu.ifsp.scl.ads.prdm.sc3039111.ImFitPlus.databinding.TileHistoryBinding
+import br.edu.ifsp.scl.ads.prdm.sc3039111.ImFitPlus.model.History
+
+class HistoryAdapter(private val historyList: MutableList<History>) : RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder>() {
+
+    inner class HistoryViewHolder(binding: TileHistoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+            val idUser = binding.idUser
+            val imcTv = binding.imcTv
+            val categoriaTv = binding.categoriaTv
+            val gastoTv = binding.gastoTv
+            val pesoIdealTv = binding.pesoIdealTv
+            val aguaTv = binding.aguaTv
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryViewHolder {
+        return HistoryViewHolder(
+            TileHistoryBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount() = historyList.size
+
+    override fun onBindViewHolder(holder: HistoryViewHolder, position: Int) {
+        val item = historyList[position]
+
+        holder.idUser.text = "ID_USUÁRIO: ${item.userId}"
+        holder.imcTv.text = "IMC: %.2f".format(item.imc)
+        holder.categoriaTv.text = "Categoria: ${item.categoria}"
+        holder.gastoTv.text = "Gasto: %.2f kcal".format(item.gasto)
+        holder.pesoIdealTv.text = "Peso ideal: %.2f kg".format(item.pesoIdeal)
+        holder.aguaTv.text = "Água/dia: %.2f L".format(item.aguaConsumo)
+    }
+}
